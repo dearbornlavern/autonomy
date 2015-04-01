faa894ec2f875bc18ee42889c457b8477da0670f
pizzamandhx <seadixon@gmail.com>
2015-04-01 15:05:16 -0600

    fixed some syntax problems, will build now.

--- ThinkGear SDK for .NET/Sample Projects/HelloEEG/HelloEEG.cs
+++ ThinkGear SDK for .NET/Sample Projects/HelloEEG/HelloEEG.cs
@@ -13,16 +13,45 @@ using NeuroSky.ThinkGear.Algorithms;
 namespace testprogram {
     class Program {
         static Connector connector;
-        static byte poorSig;   
+        static byte poorSig;
+        static bool _continue;
+        static SerialPort _serialPort;
+
 
         public static void Main(string[] args) {
             string name;
             string message;
-            StringComparer stringComparer = StringCompareer.OrdinalIgnoreCase;
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
             Thread readThread = new Thread(Read);
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort("COM5");
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
 
-            // Create a new serial port object
-            Console.WriteLine("HelloEEG!");
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Type QUIT to exit");
+
+            while (_continue)
+            {
+                message = Console.ReadLine();
+
+                if (stringComparer.Equals("quit", message))
+                {
+                    _continue = false;
+                }
+                else
+                {
+                    _serialPort.WriteLine(
+                        String.Format("<{0}>: {1}", name, message));
+                }
+            }               
+            readThread.Join();
+            _serialPort.Close();
+   
+                Console.WriteLine("HelloEEG!");
 
             // Initialize a new Connector and add event handlers
 
@@ -147,6 +176,21 @@ namespace testprogram {
 
         }
 
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
     }
 
 }
+
+
+
